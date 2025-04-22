15b680f53e0039c6754b8d2900574dfda418495e
Tor Andreas Røsæg <to.an@hotmail.no>
2025-04-22 15:31:44 +0200

    added start of interview

--- components/Article.vue
+++ components/Article.vue
@@ -4,8 +4,13 @@
 <template>
     <div class="article">
         <div class="header">
-            <h1>Lokal utvikler prøver å finne frontend jobb!</h1>
+            <h1 class="title">Lokal utvikler prøver å finne frontend jobb!</h1>
+            <h2>Med erfaring fra Microchip og master i Programutvikling, har Tor Andreas stor tro på at han skal lage løsninger folk flest får nytte av</h2>
+            <p>"Akuratt nå har jeg søkt hos polaris og håper å få jobbe med Adresseavisen, det hadde vært gøy å få jobbet for 70 000 abbonenter og blitt sett av 200 000 daglige lesere!" sier han med et smil</p>
+            <h2>Hvorfor skal de velge deg?</h2>
+            <p>Jeg er ekstremt god på å løse komplekse problemer på en enkel måte. Jeg er også svært motivert, ressurssterk og jobber godt i tverrfaglige teams. Jeg har et enormt utviklingspotensial som jeg ønsker å oppnå hos de, og hjelpe de få flere kunder og ta vare på kundene de har.</p>
             <Advertisement/>
+
         </div>
     </div>
 </template>
@@ -25,4 +30,7 @@
 
 }
 
+.title {
+    font-size: larger;
+}
 </style>
